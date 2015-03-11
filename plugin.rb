d3e59fe90540543ca727393d83669a9c7b5ec549
Naveed Ahmad <naveedahmada036@gmail.com>
2015-03-11 10:18:44 +0500

    added support for activities and modren views

--- plugin.rb
+++ plugin.rb
@@ -9,15 +9,19 @@ module Onebox
   module Engine
     class GarminConnectOnebox
       include Engine
-      REGEX = /^https?:\/\/connect\.garmin\.com(?::80)?\/course\/(?:embed\/)?([0-9]*)/
+      REGEX = /^https?:\/\/connect\.garmin\.com\/(?:modern\/)?(course|activity)\/(?:embed\/)?([0-9]*)/
       matches_regexp REGEX
 
       def id
+        @url.match(REGEX)[2]
+      end
+
+      def endpoint
         @url.match(REGEX)[1]
       end
 
       def to_html
-        "<iframe width='600' height='600' frameborder='0' src='http://connect.garmin.com/course/embed/#{id}'></iframe>"
+        "<iframe width='600' height='600' frameborder='0' src='http://connect.garmin.com/#{endpoint}/embed/#{id}'></iframe>"
       end
     end
   end
